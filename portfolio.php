063de847a0ab227e62921daeb3b0da7432958a81
Martin Mele <mmmele@gmail.com>
2022-03-08 10:16:26 -0300

    fix bugs

--- portfolio.php
+++ portfolio.php
@@ -1,175 +1,175 @@
     <!-- CONTENT -->
     <section id="portfolio">
-    	<div class="container-fluid">
-    		<div class="row no-gutters">
-    		    <!-- 1 -->
-    			<div class="col-md-4 col-6 col-lg-3">
-                    <a href="https://thekidsrepublic.com/" rel="noreferrer" target="_blank">
-    			    <div class="caja">
-                        <img src="images/tkr-thumb-2.png" alt="Project: The Kids republic" class="img-fluid">
-                        <div class="overlay bg-verde">
-                            <div class="text">Brand</div>
-                        </div>
-                    </div>
-                    </a>
-    			</div>
+        <div class="container-fluid">
+            <div class="row no-gutters">
+                <!-- 1 -->
                 <div class="col-md-4 col-6 col-lg-3">
-                    <a href="https://www.behance.net/gallery/73854405/Kanji" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio15.jpg" alt="Project: kanji app" class="img-fluid">
-                        <div class="overlay bg-agua">
-                            <div class="text">UX/UI Dev</div>
+                    <a href="https://thekidsrepublic.com/" rel="noreferrer" target="_blank">
+                        <div class="caja">
+                            <img src="images/tkr-thumb-2.png" alt="Project: The Kids republic" class="img-fluid">
+                            <div class="overlay bg-verde">
+                                <div class="text">Brand</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
                 </div>
-    			<div class="col-md-4 col-6 col-lg-3">
-                    <a href="https://www.behance.net/gallery/101568611/Administracion-de-flujos-de-trabajo" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/kpmg-thumb.png" alt="Project: KPMG" class="img-fluid">
-                        <div class="overlay bg-rojo">
-                            <div class="text">UX/UI Dev</div>
+                <div class="col-md-4 col-6 col-lg-3">
+                    <a href="https://www.behance.net/gallery/73854405/Kanji" rel="noreferrer" target="_blank">
+                        <div class="caja">
+                            <img src="images/portfolio15.jpg" alt="Project: kanji app" class="img-fluid">
+                            <div class="overlay bg-agua">
+                                <div class="text">UX/UI Dev</div>
+                            </div>
                         </div>
-                    </div>
-                    </a> 
-    			</div>
-    			<div class="col-md-4 col-6 col-lg-3">
-                    <a href="http://cocinaonlinesolenardelli.com/" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio04.jpg" alt="Project: Sole Nardelli e-learning" class="img-fluid">
-                        <div class="overlay bg-verde">
-                            <div class="text">UX/UI Dev <br>Illustration</div>
-                        </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
+                <div class="col-md-4 col-6 col-lg-3">
+                    <a href="https://www.behance.net/gallery/101568611/Administracion-de-flujos-de-trabajo" rel="noreferrer" target="_blank">
+                        <div class="caja">
+                            <img src="images/kpmg-thumb.png" alt="Project: KPMG" class="img-fluid">
+                            <div class="overlay bg-rojo">
+                                <div class="text">UX/UI Dev</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-6 col-lg-3">
+                    <a href="http://cocinaonlinesolenardelli.com/" rel="noreferrer" target="_blank">
+                        <div class="caja">
+                            <img src="images/portfolio04.jpg" alt="Project: Sole Nardelli e-learning" class="img-fluid">
+                            <div class="overlay bg-verde">
+                                <div class="text">UX/UI Dev <br>Illustration</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
             <div class="row no-gutters">
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.instagram.com/osurrin_vestite_divertite/" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio03.jpg" alt="Project: Osurrin Clothes" class="img-fluid">
-                        <div class="overlay bg-agua">
-                            <div class="text">Brand<br>Soc. Media</div>
+                        <div class="caja">
+                            <img src="images/portfolio03.jpg" alt="Project: Osurrin Clothes" class="img-fluid">
+                            <div class="overlay bg-agua">
+                                <div class="text">Brand<br>Soc. Media</div>
+                            </div>
                         </div>
-                    </div>
-                    </a> 
-    			</div>
+                    </a>
+                </div>
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://unlasoft.com.ar/" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/unlasoft-thumb.png" alt="Project: Unlasoft Dev Solutions" class="img-fluid">
-                        <div class="overlay bg-verde">
-                            <div class="text">Brand<br>UX/UI Dev</div>
+                        <div class="caja">
+                            <img src="images/unlasoft-thumb.png" alt="Project: Unlasoft Dev Solutions" class="img-fluid">
+                            <div class="overlay bg-verde">
+                                <div class="text">Brand<br>UX/UI Dev</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
-    			<div class="col-md-4 col-6 col-lg-3">
+                </div>
+                <div class="col-md-4 col-6 col-lg-3">
                     <a href="http://pressitio.com/" rel="noreferrer" target="_blank">
-    			    <div class="caja">
-                        <img src="images/portfolio05.jpg" alt="Project: Pressitio" class="img-fluid">
-                        <div class="overlay bg-rojo">
-                            <div class="text">Brand<br>UX/UI Dev</div>
+                        <div class="caja">
+                            <img src="images/portfolio05.jpg" alt="Project: Pressitio" class="img-fluid">
+                            <div class="overlay bg-rojo">
+                                <div class="text">Brand<br>UX/UI Dev</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
-    			<div class="col-md-4 col-6 col-lg-3">
+                </div>
+                <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.behance.net/gallery/101570433/Liza-Protocolos" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/liza-thumb.png" alt="Project: Liza Protocols" class="img-fluid">
-                        <div class="overlay bg-rojo">
-                            <div class="text">UX/UI Dev</div>
+                        <div class="caja">
+                            <img src="images/liza-thumb.png" alt="Project: Liza Protocols" class="img-fluid">
+                            <div class="overlay bg-rojo">
+                                <div class="text">UX/UI Dev</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
             </div>
             <div class="row no-gutters">
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.behance.net/gallery/29344859/Desvan" rel="noreferrer" target="_blank">
-    			    <div class="caja">
-                        <img src="images/portfolio11.jpg" alt="Project: Desván theater play" class="img-fluid">
-                        <div class="overlay bg-agua">
-                            <div class="text">Poster</div>
+                        <div class="caja">
+                            <img src="images/portfolio11.jpg" alt="Project: Desván theater play" class="img-fluid">
+                            <div class="overlay bg-agua">
+                                <div class="text">Poster</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
-    			<div class="col-md-4 col-6 col-lg-3">
+                </div>
+                <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.behance.net/gallery/1796116/Benzol" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio10.jpg" alt="Project: Benzol chemistry" class="img-fluid">
-                        <div class="overlay bg-verde">
-                            <div class="text">Poster</div>
+                        <div class="caja">
+                            <img src="images/portfolio10.jpg" alt="Project: Benzol chemistry" class="img-fluid">
+                            <div class="overlay bg-verde">
+                                <div class="text">Poster</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://martinmele.tumblr.com/" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio12.jpg" alt="Project: Personal Illustrations" class="img-fluid">
-                        <div class="overlay bg-rojo">
-                            <div class="text">Illustration</div>
+                        <div class="caja">
+                            <img src="images/portfolio12.jpg" alt="Project: Personal Illustrations" class="img-fluid">
+                            <div class="overlay bg-rojo">
+                                <div class="text">Illustration</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.facebook.com/fonolandia/" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio02.jpg" alt="Project: Fonolandia mobile accesories" class="img-fluid">
-                        <div class="overlay bg-agua">
-                            <div class="text">Brand<br>Soc. Media</div>
+                        <div class="caja">
+                            <img src="images/portfolio02.jpg" alt="Project: Fonolandia mobile accesories" class="img-fluid">
+                            <div class="overlay bg-agua">
+                                <div class="text">Brand<br>Soc. Media</div>
+                            </div>
                         </div>
-                    </div>
-                    </a> 
-    			</div>    			
-    		</div>
+                    </a>
+                </div>
+            </div>
             <div class="row no-gutters">
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.instagram.com/magicphonebrc/" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio14.jpg" alt="Project: Magic Phone accesories" class="img-fluid">
-                        <div class="overlay bg-rojo">
-                            <div class="text">Brand<br>Soc. Media</div>
+                        <div class="caja">
+                            <img src="images/portfolio14.jpg" alt="Project: Magic Phone accesories" class="img-fluid">
+                            <div class="overlay bg-rojo">
+                                <div class="text">Brand<br>Soc. Media</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.behance.net/gallery/49299973/LOGOFOLIO" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio09.jpg" alt="Project: Kuntur raider Adventures" class="img-fluid">
-                        <div class="overlay bg-agua">
-                            <div class="text">Brand</div>
+                        <div class="caja">
+                            <img src="images/portfolio09.jpg" alt="Project: Kuntur raider Adventures" class="img-fluid">
+                            <div class="overlay bg-agua">
+                                <div class="text">Brand</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
                 </div>
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.behance.net/gallery/2333354/Circle-of-Fifths" rel="noreferrer" target="_blank">
-                    <div class="caja">
-                        <img src="images/portfolio07.jpg" alt="Project: Circle of fifths color/music theory" class="img-fluid">
-                        <div class="overlay bg-verde">
-                            <div class="text">Poster</div>
+                        <div class="caja">
+                            <img src="images/portfolio07.jpg" alt="Project: Circle of fifths color/music theory" class="img-fluid">
+                            <div class="overlay bg-verde">
+                                <div class="text">Poster</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
                 <div class="col-md-4 col-6 col-lg-3">
                     <a href="https://www.behance.net/gallery/49299973/LOGOFOLIO" rel="noreferrer" target="_blank">
-    			    <div class="caja">
-                        <img src="images/portfolio06.jpg" alt="Project: Panacea Cocina de Autor" class="img-fluid">
-                        <div class="overlay bg-rojo">
-                            <div class="text">Brand</div>
+                        <div class="caja">
+                            <img src="images/portfolio06.jpg" alt="Project: Panacea Cocina de Autor" class="img-fluid">
+                            <div class="overlay bg-rojo">
+                                <div class="text">Brand</div>
+                            </div>
                         </div>
-                    </div>
                     </a>
-    			</div>
+                </div>
             </div>
-    	</div>
+        </div>
     </section>
-	<!-- END CONTENT -->
+    <!-- END CONTENT -->
