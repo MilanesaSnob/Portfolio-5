758bd4b430ce4393518f103d268fd9d704760f55
Martin Mele <mmmele@gmail.com>
2022-03-09 13:12:23 -0300

    Variante estilos

---
+++ index_2.php
@@ -0,0 +1,19 @@
+<?php
+// Logica para capturar la pantalla que queremos abrir
+$pagina = isset($_GET['p']) ? strtolower($_GET['p']) : 'home';
+
+// Agrego cabecera
+require_once 'header_2.php';
+
+// Estoy usando require en vez de include, el parámetro enviado tiene el mismo nombre del archivo a cargar
+require_once $pagina . '.php';
+
+// Agrego pie
+require_once 'footer.php';
+
+
+?>
+
+
+
+    
